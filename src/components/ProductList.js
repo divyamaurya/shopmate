822a1f06b9eebe0c5bf69d3749d0e619b7d2e54f
Divya <divyamaurya025@gmail.com>
2024-02-05 18:23:52 +0530

    changed fake api to local server, design change and filter using api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,20 +3,42 @@ import "./productlist.css";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [url, setUrl] = useState("http://localhost:8000/products");
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+    fetch(url)
       .then((response) => response.json())
       .then((data) => setProducts(data));
-  }, []);
+    // console.log(products);
+  }, [url]);
 
   return (
     <section>
+      <div>
+        <button
+          className="btn"
+          onClick={() => setUrl("http://localhost:8000/products")}
+        >
+          All
+        </button>
+        <button
+          className="btn"
+          onClick={() => setUrl("http://localhost:8000/products?available=1")}
+        >
+          In Stock
+        </button>
+      </div>
+
       {products.map((product) => (
         <div className="card" key={product.id}>
-          <p>{product.title}</p>
-          <img src={product.image} alt={product.title} />
-          <p>${product.price}</p>
+          <div>
+            <p>ProductID: {product.id}</p>
+            <p>{product.name}</p>
+          </div>
+          <div>
+            <p>${product.price}</p>
+          </div>
+          {product.available === true ? "Available" : "Unavailable"}
         </div>
       ))}
     </section>
